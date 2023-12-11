7fa4bfc677638a0913c06fedd369d4a784b06227
Anton Andreev <rawkward@yandex.ru>
2023-12-11 21:43:26 +0700

    added createCard function

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,22 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const cardContainer = document.querySelector('.places__list');
 
-function addCards(initCardsArray, deleteFunction) {
+function createCard(item, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button'); 
+  cardImage.setAttribute('src', item.link);
+  cardImage.setAttribute('alt', `Изображение локации ${item.name}`);
+  cardTitle.textContent = item.name;
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+  return cardElement;
+}
+
+function addCards(initCardsArray, deleteCard) {
   initCardsArray.forEach((item) => {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const cardImage = cardElement.querySelector('.card__image');
-    const cardTitle = cardElement.querySelector('.card__title');
-    const deleteButton = cardElement.querySelector('.card__delete-button');
-    cardImage.setAttribute('src', item.link);
-    cardImage.setAttribute('alt', `Изображение локации ${item.name}`);
-    cardTitle.textContent = item.name;
+    const cardElement = createCard(item, deleteCard);
     cardContainer.append(cardElement);
-    deleteButton.addEventListener('click', function () {
-      deleteFunction(cardElement);
-    });
   });
 }
 
