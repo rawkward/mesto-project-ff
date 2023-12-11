7967d969f6f09ed4c8c80285244ac8b66de83d45
Anton Andreev <rawkward@yandex.ru>
2023-12-11 16:43:41 +0700

    added addCard function

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
+
+function addCards(initCardsArray/*, removeCard*/) {
+    initCardsArray.forEach(item => {
+        const card = cardTemplate.querySelector('.card').cloneNode(true);
+        const cardImage = card.querySelector('.card__image');
+        const cardTitle = card.querySelector('.card__title');
+        cardImage.setAttribute('src', item.link);
+        cardTitle.textContent = item.name;
+        cardContainer.append(card);
+    });
+}
+
+addCards(initialCards);
+
+
+
+
+
+
+
+
+
 
 // @todo: DOM узлы
 
